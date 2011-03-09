6ba4f9ff5b5a83d7f2854d06aa8256fe70d843f0
shaw198338@gmail.com <shaw198338@gmail.com@bdf23ff4-39da-c976-f7a5-473e8ee853b7>
2011-03-09 18:09:00 +0000

    Modified main.c to run a simple code for thread join and mutex. It works.
    Yang
    
    M    main.c
    M    makefile

--- trunk/main.c
+++ trunk/main.c
@@ -1,4 +1,3 @@
-// 
 // **************************************************************
 // Function: 	main	
 // Description:	Used to run the code
@@ -7,36 +6,97 @@
 #include <stdlib.h>
 #include <stdio.h>
 
-#define PROCESSOR_NUM 10
 
+// Function declearation
 int assoc_gen(int threshold);
-void *print_message_function( void *ptr );
+
+// Global variables declearation
+#define PROCESSOR_NUM 5
+pthread_mutex_t mutexsum;
+
+int global_array[5][5];
+
+
+void *find_large_item(void *tid)
+{
+	int i, j;
+	int cell;
+	int local_array[5][5];
+		
+	cell = (int)((long) tid);
+	
+	printf("Thread ID: %d\n", cell);
+
+	for (i = 0; i < 5; i++)
+	{
+		for (j = 0; j < 5; j++)
+		{
+			local_array[i][j] = cell;
+		}
+	}	
+	printf("Thread ID Merge: %d\n", cell);
+	
+	pthread_mutex_lock(&mutexsum);
+	for (i = 0; i < 5; i++)
+	{
+		for (j = 0; j < 5; j++)
+		{
+			global_array[i][j] = global_array[i][j] + local_array[i][j];
+		}
+	}	
+	pthread_mutex_unlock(&mutexsum);
+
+	pthread_exit((void*) 0);
+}
+
 
 int main(void)
 {
 	int i = 0;
+	int j = 0;
 	int threshold = 0;
-	assoc_gen(threshold);
-     
-	
-
+	void *status;
+	//assoc_gen(threshold);
+    pthread_attr_t attr;
 	pthread_t thread[PROCESSOR_NUM];
+	
+	// Initialize the global array
+	for (i = 0; i < 5; i++)
+	{
+		for (j = 0; j < 5; j++)
+		{
+			global_array[i][j] = 0;
+		}
+	}	
+
+	pthread_mutex_init(&mutexsum, NULL);
+	pthread_attr_init(&attr);
+	pthread_attr_setdetachstate(&attr, PTHREAD_CREATE_JOINABLE);
 	
 	for (i = 0; i < PROCESSOR_NUM; i++)
 	{
-		pthread_create( &thread[i], NULL, print_message_function, ref);
-
+		pthread_create(&thread[i], &attr, find_large_item, (void *)((long) i));
 	}
+	
+	pthread_attr_destroy(&attr);
 
-     exit(0);
+	for (i = 0; i < PROCESSOR_NUM; i++)
+	{
+		pthread_join(thread[i], &status);
+	}
+			
+	for (i = 0; i < 5; i++)
+	{
+		for (j = 0; j < 5; j++)
+		{
+			printf("%d\t", global_array[i][j]);
+		}
+		printf("\n");
+	}	
+	pthread_mutex_destroy(&mutexsum);
+	pthread_exit(NULL);
 }
 
 
-void *print_message_function( void *ptr )
-{
-     char *message;
-     message = (char *) ptr;
-     printf("%s \n", message);
-}
 
 	
