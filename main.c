1037bbcd464ff6fa78f751eda5497643abf5b685
diwangbruce@gmail.com <diwangbruce@gmail.com@bdf23ff4-39da-c976-f7a5-473e8ee853b7>
2011-03-12 17:23:15 +0000

    added barrier for synchronization on global_array, fixed a print bug on DEBUG_2 level on global array.

--- main.c
+++ main.c
@@ -32,9 +32,16 @@ int main(void)
 		printf("ITER_NUM: %d, PROCESSOR_NUM: %d\n", ITER_NUM, PROCESSOR_NUM);	
 	}
 
-    pthread_attr_t attr;
+    	pthread_attr_t attr;
 	pthread_t thread[PROCESSOR_NUM];
-	
+		
+	// Barrier initialization
+	if (pthread_barrier_init(&barr, NULL, PROCESSOR_NUM))
+	{
+		printf("Could not creat a barrier\n");
+		return -1;
+	}	
+
 	// Initialize the global array
 	for (i = 0; i < ARRAY_ROW_NUM; i++)
 	{
@@ -55,14 +62,22 @@ int main(void)
 	
 	for (i = 0; i < PROCESSOR_NUM; i++)
 	{
-		pthread_create(&thread[i], &attr, find_large_item, (void *)((long) i));
+		if (pthread_create(&thread[i], &attr, find_large_item, (void *)((long) i)))
+		{
+			printf("Could not create thread %d\n", i);
+			return -1;
+		}
 	}
 	
 	pthread_attr_destroy(&attr);
 
 	for (i = 0; i < PROCESSOR_NUM; i++)
 	{
-		pthread_join(thread[i], &status);
+		if (pthread_join(thread[i], &status))
+		{
+			printf("Could not join thread %d\n", i);
+			return -1;
+		}
 	}
 		
 	pthread_mutex_destroy(&mutexsum);
@@ -75,7 +90,7 @@ int main(void)
 		{
 			for (j = 0; j < ARRAY_COL_NUM; j++)
 			{
-				if (global_array[i][j].counter);
+				if (global_array[i][j].counter)
 				{
 					printf("Combination '%c%c' happen %d times at iteration: \n", i + 97, j + 97, global_array[i][j].counter);
 					for (k = 0; k < ITER_NUM; k++)
